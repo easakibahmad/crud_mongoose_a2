ff56676c9da44df8bf35fb67955a8dd50e49ce8a
easakibahmad <sakib.sgc.du@gmail.com>
2023-11-23 05:31:29 +0600

    list of all ussers api created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { userServices } from "./user.service";
+import { UserModel } from "../user.model";
 
 const createUser = async (req: Request, res: Response) => {
   try {
@@ -39,6 +40,35 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await UserModel.aggregate([
+      {
+        $project: {
+          username: 1,
+          fullName: 1,
+          age: 1,
+          email: 1,
+          address: 1,
+          _id: 0,
+        },
+      },
+    ]);
+    res.status(200).json({
+      success: true,
+      message: "Users fetched successfully!",
+      data: users,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
 export const userController = {
   createUser,
+  getAllUsers,
 };
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,5 +4,6 @@ import { userController } from "./user.controller";
 const router = express.Router();
 
 router.post("/users", userController.createUser);
+router.get("/users", userController.getAllUsers);
 
 export const userRoutes = router;
