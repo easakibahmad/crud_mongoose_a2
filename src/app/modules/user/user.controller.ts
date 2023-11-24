a9098b511f4515329a167dd5eda67286929f65c5
easakibahmad <sakib.sgc.du@gmail.com>
2023-11-25 03:05:09 +0600

    validate updated fields when update data with zod

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,6 +3,7 @@ import { userServices } from "./user.service";
 import {
   UserValidationSchema,
   OrderValidatorSchema,
+  UserUpdateValidationSchema,
 } from "./user.zod.validation";
 
 //api to create user
@@ -100,44 +101,62 @@ const updateSingleUser = async (req: Request, res: Response) => {
   try {
     const userId = req.params.userId;
     const updatedData = req.body;
+    const zodParsedData = UserUpdateValidationSchema.parse(updatedData); //validate with partial fields for updating user data with zod
+
+    const updatedDataKeys = Object.keys(updatedData); // check updated data keys
+    const schemaKeys = Object.keys(UserUpdateValidationSchema.shape); //check schema keys
+    const invalidKeys = updatedDataKeys.filter(
+      (key) => !schemaKeys.includes(key)
+    ); //get invalid keys
     const updatedUserId = updatedData?.userId;
     const result = await userServices.updateSingleUserFromDB(
       userId,
-      updatedData
+      zodParsedData
     );
 
-    if (result) {
-      // if userId is updated
-      if (updatedUserId) {
-        const updatedUserNewData = await userServices.getSingleUserFromDB(
-          updatedUserId
-        );
-        res.status(200).json({
-          success: true,
-          message: "User updated successfully!",
-          data: updatedUserNewData,
-        });
-      }
-      // if userId is not updated
-      else {
-        const updatedUserNewData = await userServices.getSingleUserFromDB(
-          userId
-        );
-        res.status(200).json({
-          success: true,
-          message: "User updated successfully!",
-          data: updatedUserNewData,
-        });
-      }
-    } else {
+    // if invalid keys length is greater then 0
+    if (invalidKeys.length > 0) {
       res.status(404).json({
         success: false,
-        message: "User not found",
-        error: {
-          code: 404,
-          description: "User not found!",
-        },
+        message: "Invalid keys found",
       });
+      return;
+    }
+    //if not found invalid keys
+    else {
+      if (result) {
+        // if userId is updated
+        if (updatedUserId) {
+          const updatedUserNewData = await userServices.getSingleUserFromDB(
+            updatedUserId
+          );
+          res.status(200).json({
+            success: true,
+            message: "User updated successfully!",
+            data: updatedUserNewData,
+          });
+        }
+        // if userId is not updated
+        else {
+          const updatedUserNewData = await userServices.getSingleUserFromDB(
+            userId
+          );
+          res.status(200).json({
+            success: true,
+            message: "User updated successfully!",
+            data: updatedUserNewData,
+          });
+        }
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "User not found",
+          error: {
+            code: 404,
+            description: "User not found!",
+          },
+        });
+      }
     }
   } catch (error) {
     res.status(404).json({
--- src/app/modules/user/user.zod.validation.ts
+++ src/app/modules/user/user.zod.validation.ts
@@ -34,4 +34,39 @@ export const OrderValidatorSchema = z.object({
   productName: z.string(),
   price: z.number(),
   quantity: z.number(),
-});
+} );
+
+
+export const UserUpdateValidationSchema = z
+  .object({
+    userId: z.number().optional(),
+    username: z.string().optional(),
+    password: z.string().optional(),
+    fullName: z
+      .object({
+        firstName: z.string().max(20).optional(),
+        lastName: z.string().max(20).optional(),
+      })
+      .optional(),
+    age: z.number().optional(),
+    email: z.string().email().optional(),
+    isActive: z.boolean().optional(),
+    hobbies: z.array(z.string()).optional(),
+    address: z
+      .object({
+        street: z.string().optional(),
+        city: z.string().optional(),
+        country: z.string().optional(),
+      })
+      .optional(),
+    orders: z
+      .array(
+        z.object({
+          productName: z.string().optional(),
+          price: z.number().optional(),
+          quantity: z.number().optional(),
+        })
+      )
+      .default([]),
+  })
+  .partial();
